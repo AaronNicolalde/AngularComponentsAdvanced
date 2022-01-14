cbe8ac97284b812bb225ff9e6a28108a89835d7c
AaronNicolalde <eduanico@espol.edu.ec>
2022-01-14 16:42:09 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgressBarComponent } from "./progress-bar/progress-bar.component";
+import { DisplayComponent } from "./display/display.component";
+import { TimerComponent } from './timer/timer.component';
+import { AlertViewComponent } from './alert-view/alert-view.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tab/tab.component';
+import { SimpleAlertViewComponent } from './simple-alert-view/simple-alert-view.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressBarComponent,
+    DisplayComponent,
+    TimerComponent,
+    AlertViewComponent,
+    TabsComponent,
+    TabComponent,
+    SimpleAlertViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule
+  ],
+  entryComponents: [
+    SimpleAlertViewComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
